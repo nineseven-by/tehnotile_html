dca0fae7334c494e09fbbe3d3de74afb214f90c8
Pavelbox <pavv.inbox@gmail.com>
2019-07-25 17:41:25 +0300

    bugfix#4

--- app/js/interface.js
+++ app/js/interface.js
@@ -648,34 +648,34 @@ function revealOnScroll() {
 
 
 // links pages
-// $('body').append(
-// 	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; background: #fff; border: solid 1px #828286; width: 200px;"> \
-// 		<a href="javascript:void(0);" style="float: right;background:#ccc; color:#000; padding: 5px 10px; text-decoration: none; font-size: 16px" onclick="$(this).parent().hide()">Close X</a> \
-// 	<style> \
-// 		#pages { padding: 10px 10px 0 30px; font-size: 16px; } \
-// 		#pages a { text-decoration: none; } \
-// 		#pages li { margin: 5px 0; } \
-// 	</style> \
-// 	<ol id="pages"> \
-// 		<li><a href="news.html">News</a></li> \
-// 		<li><a href="search.html">Search</a></li> \
-// 		<li><a href="reg.html">Registration</a></li> \
-// 		<li><a href="profile1.html">Profile1</a></li> \
-// 		<li><a href="profile2.html">Profile2</a></li> \
-// 		<li><a href="profile3.html">Profile3</a></li> \
-// 		<li><a href="profile4.html">Profile4</a></li> \
-// 		<li><a href="oformit.html">Oformit</a></li> \
-// 		<li><a href="page404.html">Page404</a></li> \
-// 		<li><a href="catalog.html">Catalog</a></li> \
-// 		<li><a href="history.html">History</a></li> \
-// 		<li><a href="favorite.html">Favorite</a></li> \
-// 		<li><a href="text.html">Text</a></li> \
-// 		<li><a href="index.html">Index</a></li> \
-// 		<li><a href="plitka.html">Plitka</a></li> \
-// 		<li><a href="cart.html">Korzina</a></li> \
-// 		<li><a href="product.html">Product</a></li> \
-// 		<li><a href="menu.html">Menu</a></li> \
-// 		<li><a href="sravnit.html">Sravnit</a></li> \
-// 		<li><a href="interior.html">Interior</a></li> \
-// 	</ol> \
-// </div>');
+$('body').append(
+	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; background: #fff; border: solid 1px #828286; width: 200px;"> \
+		<a href="javascript:void(0);" style="float: right;background:#ccc; color:#000; padding: 5px 10px; text-decoration: none; font-size: 16px" onclick="$(this).parent().hide()">Close X</a> \
+	<style> \
+		#pages { padding: 10px 10px 0 30px; font-size: 16px; } \
+		#pages a { text-decoration: none; } \
+		#pages li { margin: 5px 0; } \
+	</style> \
+	<ol id="pages"> \
+		<li><a href="news.html">News</a></li> \
+		<li><a href="search.html">Search</a></li> \
+		<li><a href="reg.html">Registration</a></li> \
+		<li><a href="profile1.html">Profile1</a></li> \
+		<li><a href="profile2.html">Profile2</a></li> \
+		<li><a href="profile3.html">Profile3</a></li> \
+		<li><a href="profile4.html">Profile4</a></li> \
+		<li><a href="oformit.html">Oformit</a></li> \
+		<li><a href="page404.html">Page404</a></li> \
+		<li><a href="catalog.html">Catalog</a></li> \
+		<li><a href="history.html">History</a></li> \
+		<li><a href="favorite.html">Favorite</a></li> \
+		<li><a href="text.html">Text</a></li> \
+		<li><a href="index.html">Index</a></li> \
+		<li><a href="plitka.html">Plitka</a></li> \
+		<li><a href="cart.html">Korzina</a></li> \
+		<li><a href="product.html">Product</a></li> \
+		<li><a href="menu.html">Menu</a></li> \
+		<li><a href="sravnit.html">Sravnit</a></li> \
+		<li><a href="interior.html">Interior</a></li> \
+	</ol> \
+</div>');
